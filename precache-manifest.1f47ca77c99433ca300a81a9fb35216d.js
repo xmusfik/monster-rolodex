b824d66d0be7e4f103116a1c609ee9b9ee8a1456
musfik <you@example.com>
2020-09-19 02:20:35 +0600

    Updates

---
+++ precache-manifest.1f47ca77c99433ca300a81a9fb35216d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e56cf215b38880ecb0e25ea0c049344",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "53f3d301beb0efbfec2c",
+    "url": "/monster-rolodex/static/css/main.2697c3cb.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53f3d301beb0efbfec2c",
+    "url": "/monster-rolodex/static/js/main.79d2f48c.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  }
+]);
